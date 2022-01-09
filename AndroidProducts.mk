d272ebdeafcd658b14d514c17c0e209333682197
AdarshGrewal <adarshgrewal@gmail.com>
2022-01-09 16:32:36 +0530

    Plus !

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_lemonades.mk
+    $(LOCAL_DIR)/plus_lemonades.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_lemonades-user \
-    lineage_lemonades-userdebug \
-    lineage_lemonades-eng
+    plus_lemonades-user \
+    plus_lemonades-userdebug \
+    plus_lemonades-eng
